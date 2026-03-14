f97faa7b4dbc6b58ef8bc071ca58fdf217ccb726
Hillariot <hillariot2070@gmail.com>
2026-03-14 16:25:02 +0400

    Update 0 - upload

---
+++ SatteliteManagment/SatteliteManagment/DuplexTcpClient.cs
@@ -0,0 +1,59 @@
+﻿using SBandSerialReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SatteliteManagment
+{
+    internal class DuplexTcpClient
+    {
+        private TcpClient _client;
+        private NetworkStream _stream;
+        private CancellationTokenSource _cts;
+
+        public event Action<string> TextReceived;
+
+        public async Task ConnectAsync(string ip, int port)
+        {
+            _client = new TcpClient();
+            await _client.ConnectAsync(ip, port);
+            _stream = _client.GetStream();
+            _cts = new CancellationTokenSource();
+
+            _ = ReceiveLoop(_cts.Token);
+        }
+
+        public async Task SendTextAsync(string text, CancellationToken token = default)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(text);
+            await PacketProtocol.WritePacketAsync(_stream, data, token);
+        }
+
+        private async Task ReceiveLoop(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    byte[] data = await PacketProtocol.ReadPacketAsync(_stream, token);
+                    string text = Encoding.ASCII.GetString(data);
+                    TextReceived?.Invoke(text);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public void Close()
+        {
+            try { _cts?.Cancel(); } catch { }
+            try { _stream?.Close(); } catch { }
+            try { _client?.Close(); } catch { }
+        }
+    }
+}
---
+++ SatteliteManagment/SatteliteManagment/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SatteliteManagment
+{
+    public partial class Form1 : Form
+    {
+        private readonly DuplexTcpClient _client = new DuplexTcpClient();
+        public Form1()
+        {
+            InitializeComponent();
+
+            _client.TextReceived += text =>
+            {
+                BeginInvoke(new Action(() =>
+                {
+                    sentTextBox.AppendText("С сервера: " + text + Environment.NewLine);
+                }));
+            };
+        }
+
+        private async Task startListeningServer()
+        {
+            try
+            {
+                await _client.ConnectAsync("127.0.0.1", 8924);
+                MessageBox.Show("Подключение к серверу выполнено");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectToServer_Click(object sender, EventArgs e)
+        {
+            startListeningServer();
+        }
+    }
+}
