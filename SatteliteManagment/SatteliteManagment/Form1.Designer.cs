558b7df2adcd76fd9e2da5d78c8f744d2da3bf56
Hillariot <hillariot2070@gmail.com>
2026-03-20 19:19:09 +0400

    Update 1 - console + read bytes from file

--- SatteliteManagment/SatteliteManagment/Form1.Designer.cs
+++ SatteliteManagment/SatteliteManagment/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace SatteliteManagment
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SatteliteManagment
 {
     partial class Form1
     {
@@ -20,55 +23,295 @@
             base.Dispose(disposing);
         }
 
-        #region Код, автоматически созданный конструктором форм Windows
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.sentTextBox = new System.Windows.Forms.TextBox();
-            this.connectToServer = new System.Windows.Forms.Button();
+            this.buttonClearLogs = new System.Windows.Forms.Button();
+            this.dataGridViewLogs = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelSnrInfoB = new System.Windows.Forms.Label();
+            this.label61 = new System.Windows.Forms.Label();
+            this.radioButtonSeparatorNothing = new System.Windows.Forms.RadioButton();
+            this.radioButtonSeparatorDollar = new System.Windows.Forms.RadioButton();
+            this.buttonSendCommand = new System.Windows.Forms.Button();
+            this.textBoxSendCommand = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelSnrInfoA = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonOpenCloseServer = new System.Windows.Forms.Button();
+            this.labelComPortConnectionInfo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelRadioControl = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLogs)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelRadioControl.SuspendLayout();
             this.SuspendLayout();
             // 
-            // sentTextBox
+            // buttonClearLogs
             // 
-            this.sentTextBox.Location = new System.Drawing.Point(136, 54);
-            this.sentTextBox.Multiline = true;
-            this.sentTextBox.Name = "sentTextBox";
-            this.sentTextBox.Size = new System.Drawing.Size(511, 280);
-            this.sentTextBox.TabIndex = 0;
-            this.sentTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.buttonClearLogs.Location = new System.Drawing.Point(993, 3);
+            this.buttonClearLogs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonClearLogs.Name = "buttonClearLogs";
+            this.buttonClearLogs.Size = new System.Drawing.Size(83, 23);
+            this.buttonClearLogs.TabIndex = 4;
+            this.buttonClearLogs.Text = "Очистить";
+            this.buttonClearLogs.UseVisualStyleBackColor = true;
+            this.buttonClearLogs.Click += new System.EventHandler(this.buttonClearLogs_Click);
             // 
-            // connectToServer
+            // dataGridViewLogs
             // 
-            this.connectToServer.Location = new System.Drawing.Point(653, 54);
-            this.connectToServer.Name = "connectToServer";
-            this.connectToServer.Size = new System.Drawing.Size(109, 23);
-            this.connectToServer.TabIndex = 1;
-            this.connectToServer.Text = "Connect to server";
-            this.connectToServer.UseVisualStyleBackColor = true;
-            this.connectToServer.Click += new System.EventHandler(this.connectToServer_Click);
+            this.dataGridViewLogs.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewLogs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLogs.Location = new System.Drawing.Point(243, 30);
+            this.dataGridViewLogs.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridViewLogs.Name = "dataGridViewLogs";
+            this.dataGridViewLogs.RowHeadersWidth = 51;
+            this.dataGridViewLogs.RowTemplate.Height = 29;
+            this.dataGridViewLogs.Size = new System.Drawing.Size(832, 308);
+            this.dataGridViewLogs.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelSnrInfoB);
+            this.groupBox1.Controls.Add(this.label61);
+            this.groupBox1.Controls.Add(this.radioButtonSeparatorNothing);
+            this.groupBox1.Controls.Add(this.radioButtonSeparatorDollar);
+            this.groupBox1.Controls.Add(this.buttonSendCommand);
+            this.groupBox1.Controls.Add(this.textBoxSendCommand);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.labelSnrInfoA);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.buttonOpenCloseServer);
+            this.groupBox1.Controls.Add(this.labelComPortConnectionInfo);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(9, 3);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(230, 196);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Подключение и данные";
+            // 
+            // labelSnrInfoB
+            // 
+            this.labelSnrInfoB.AutoSize = true;
+            this.labelSnrInfoB.Location = new System.Drawing.Point(137, 51);
+            this.labelSnrInfoB.Name = "labelSnrInfoB";
+            this.labelSnrInfoB.Size = new System.Drawing.Size(13, 13);
+            this.labelSnrInfoB.TabIndex = 15;
+            this.labelSnrInfoB.Text = "0";
+            // 
+            // label61
+            // 
+            this.label61.AutoSize = true;
+            this.label61.Location = new System.Drawing.Point(93, 51);
+            this.label61.Name = "label61";
+            this.label61.Size = new System.Drawing.Size(45, 13);
+            this.label61.TabIndex = 14;
+            this.label61.Text = "RSSI B:";
+            // 
+            // radioButtonSeparatorNothing
+            // 
+            this.radioButtonSeparatorNothing.AutoSize = true;
+            this.radioButtonSeparatorNothing.Location = new System.Drawing.Point(4, 153);
+            this.radioButtonSeparatorNothing.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButtonSeparatorNothing.Name = "radioButtonSeparatorNothing";
+            this.radioButtonSeparatorNothing.Size = new System.Drawing.Size(138, 17);
+            this.radioButtonSeparatorNothing.TabIndex = 13;
+            this.radioButtonSeparatorNothing.Text = "Без разделителя байт";
+            this.radioButtonSeparatorNothing.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonSeparatorDollar
+            // 
+            this.radioButtonSeparatorDollar.AutoSize = true;
+            this.radioButtonSeparatorDollar.Checked = true;
+            this.radioButtonSeparatorDollar.Location = new System.Drawing.Point(4, 134);
+            this.radioButtonSeparatorDollar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButtonSeparatorDollar.Name = "radioButtonSeparatorDollar";
+            this.radioButtonSeparatorDollar.Size = new System.Drawing.Size(142, 17);
+            this.radioButtonSeparatorDollar.TabIndex = 12;
+            this.radioButtonSeparatorDollar.TabStop = true;
+            this.radioButtonSeparatorDollar.Text = "Разделитель байт - \"$\"";
+            this.radioButtonSeparatorDollar.UseVisualStyleBackColor = true;
+            // 
+            // buttonSendCommand
+            // 
+            this.buttonSendCommand.Location = new System.Drawing.Point(145, 134);
+            this.buttonSendCommand.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSendCommand.Name = "buttonSendCommand";
+            this.buttonSendCommand.Size = new System.Drawing.Size(81, 35);
+            this.buttonSendCommand.TabIndex = 11;
+            this.buttonSendCommand.Text = "Отправить\r\nкоманду";
+            this.buttonSendCommand.UseVisualStyleBackColor = true;
+            this.buttonSendCommand.Click += new System.EventHandler(this.buttonSendCommand_Click);
+            // 
+            // textBoxSendCommand
+            // 
+            this.textBoxSendCommand.Location = new System.Drawing.Point(4, 112);
+            this.textBoxSendCommand.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxSendCommand.Name = "textBoxSendCommand";
+            this.textBoxSendCommand.Size = new System.Drawing.Size(222, 20);
+            this.textBoxSendCommand.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Отправка команд:";
+            // 
+            // labelSnrInfoA
+            // 
+            this.labelSnrInfoA.AutoSize = true;
+            this.labelSnrInfoA.Location = new System.Drawing.Point(49, 51);
+            this.labelSnrInfoA.Name = "labelSnrInfoA";
+            this.labelSnrInfoA.Size = new System.Drawing.Size(13, 13);
+            this.labelSnrInfoA.TabIndex = 8;
+            this.labelSnrInfoA.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "RSSI A:";
+            // 
+            // buttonOpenCloseServer
+            // 
+            this.buttonOpenCloseServer.Location = new System.Drawing.Point(4, 65);
+            this.buttonOpenCloseServer.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonOpenCloseServer.Name = "buttonOpenCloseServer";
+            this.buttonOpenCloseServer.Size = new System.Drawing.Size(220, 29);
+            this.buttonOpenCloseServer.TabIndex = 6;
+            this.buttonOpenCloseServer.Text = "Включить сервер";
+            this.buttonOpenCloseServer.UseVisualStyleBackColor = true;
+            this.buttonOpenCloseServer.Click += new System.EventHandler(this.connectToServer_Click);
+            // 
+            // labelComPortConnectionInfo
+            // 
+            this.labelComPortConnectionInfo.AutoSize = true;
+            this.labelComPortConnectionInfo.Location = new System.Drawing.Point(75, 23);
+            this.labelComPortConnectionInfo.Name = "labelComPortConnectionInfo";
+            this.labelComPortConnectionInfo.Size = new System.Drawing.Size(85, 13);
+            this.labelComPortConnectionInfo.TabIndex = 5;
+            this.labelComPortConnectionInfo.Text = "Не подключено";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(49, 21);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(21, 18);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Статус:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Лог:";
+            // 
+            // panelRadioControl
+            // 
+            this.panelRadioControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelRadioControl.Controls.Add(this.button1);
+            this.panelRadioControl.Controls.Add(this.label1);
+            this.panelRadioControl.Controls.Add(this.groupBox1);
+            this.panelRadioControl.Controls.Add(this.dataGridViewLogs);
+            this.panelRadioControl.Controls.Add(this.buttonClearLogs);
+            this.panelRadioControl.Location = new System.Drawing.Point(0, 19);
+            this.panelRadioControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panelRadioControl.Name = "panelRadioControl";
+            this.panelRadioControl.Size = new System.Drawing.Size(1085, 340);
+            this.panelRadioControl.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Чтение команд из файла";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text files (*.txt)|*.txt";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.connectToServer);
-            this.Controls.Add(this.sentTextBox);
+            this.ClientSize = new System.Drawing.Size(1085, 361);
+            this.Controls.Add(this.panelRadioControl);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "SatteliteManagment";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLogs)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelRadioControl.ResumeLayout(false);
+            this.panelRadioControl.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox sentTextBox;
-        private System.Windows.Forms.Button connectToServer;
+        private CheckBox checkBoxDollar;
+        private Button buttonClearLogs;
+        private DataGridView dataGridViewLogs;
+        private GroupBox groupBox1;
+        public Label labelSnrInfoB;
+        private Label label61;
+        private RadioButton radioButtonSeparatorNothing;
+        private RadioButton radioButtonSeparatorDollar;
+        private Button buttonSendCommand;
+        private TextBox textBoxSendCommand;
+        private Label label6;
+        public Label labelSnrInfoA;
+        private Label label5;
+        public Button buttonOpenCloseServer;
+        public Label labelComPortConnectionInfo;
+        public PictureBox pictureBox1;
+        private Label label4;
+        private Label label1;
+        private Panel panelRadioControl;
+        private Button button1;
+        private OpenFileDialog openFileDialog1;
     }
 }
 
--- SatteliteManagment/SatteliteManagment/Form1.cs
+++ SatteliteManagment/SatteliteManagment/Form1.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ namespace SatteliteManagment
 {
     public partial class Form1 : Form
     {
+
         private readonly DuplexTcpClient _client = new DuplexTcpClient();
         public Form1()
         {
@@ -21,32 +25,157 @@ namespace SatteliteManagment
             {
                 BeginInvoke(new Action(() =>
                 {
-                    sentTextBox.AppendText("С сервера: " + text + Environment.NewLine);
+                    //sentTextBox.AppendText("С сервера: " + text + Environment.NewLine);
                 }));
             };
         }
 
-        private async Task startListeningServer()
+        void changeInterfaceState(bool stateServer)
         {
+            if (stateServer)
+            {
+                pictureBox1.BackColor = Color.Green;
+                buttonOpenCloseServer.Text = "Выключить сервер";
+            }
+            else
+            {
+                pictureBox1.BackColor = Color.Red;
+                buttonOpenCloseServer.Text = "Включить сервер";
+            }
+        }
+
+        private async Task changeServerState(bool stateServer)
+        {
+
+            //true - включить сервер, false - выключить сервер
+            if (stateServer)
+                try
+                {
+                    buttonOpenCloseServer.Enabled = false;
+                    await _client.ConnectAsync("127.0.0.1", 8924);
+                    changeInterfaceState(true);
+                    buttonOpenCloseServer.Enabled = true;
+
+                }
+                catch (Exception e)
+                {
+                        MessageBox.Show("Подключение к серверу не было выполнено. Проверьте, что сервер включен");
+                    buttonOpenCloseServer.Enabled = true;
+                    changeInterfaceState(false);
+                }
+            else
+                changeInterfaceState(false);
+        }
+
+        private void SetDataGridLogHeaders()
+        {
+            dataGridViewLogs.ColumnCount = 6;
+            dataGridViewLogs.Columns[0].Width = 165;
+            dataGridViewLogs.Columns[1].Width = 100;
+            dataGridViewLogs.Columns[2].Width = 70;
+            dataGridViewLogs.Columns[3].Width = 70;
+            dataGridViewLogs.Columns[4].Width = 305;
+            dataGridViewLogs.Columns[5].Width = 355;
+
+            dataGridViewLogs.Columns[0].HeaderText = "Дата и время";
+            dataGridViewLogs.Columns[1].HeaderText = "Номер приёмника";
+            dataGridViewLogs.Columns[2].HeaderText = "SNR";
+            dataGridViewLogs.Columns[3].HeaderText = "RSSI";
+            dataGridViewLogs.Columns[4].HeaderText = "Байтовый текст";
+            dataGridViewLogs.Columns[5].HeaderText = "Текстовое описание пакета";
+        }
+
+        private void buttonSendCommand_Click(object sender, EventArgs e)
+        {/*
+            if (!serialPort.IsOpen)
+            {
+                MessageBox.Show("COM порт закрыт!");
+                return;
+            }
+
             try
             {
-                await _client.ConnectAsync("127.0.0.1", 8924);
-                MessageBox.Show("Подключение к серверу выполнено");
+                string commandToSendRaw = textBoxSendCommand.Text;
+
+                string commandToSend;
+                if (radioButtonSeparatorDollar.Checked)
+                {
+                    commandToSend = commandToSendRaw.Replace("$", String.Empty);
+                }
+                else
+                {
+                    commandToSend = commandToSendRaw;
+                }
+
+                byte[] commandToSendBytes = GetBytesFromByteString(commandToSend).ToArray();
+
+                outputQueue.Add(commandToSendBytes);
+
+                ColoredItem outputLog = new ColoredItem(commandToSendBytes, "Отправка команды с терминала", Color.Blue);
+                WriteRadioControlLog(outputLog);
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                MessageBox.Show(e.ToString());
+                MessageBox.Show("Не удалось преобразовать строку в команду, проверьте правильность ввода");
+            }
+            */
+        }
+
+        public void WriteRadioControlLog(/*ColoredItem item*/)
+        {
+            /*
+            if (item.color == Color.Green && !checkBoxIsLoggingTrancieverChanges.Checked)
+            {
+                return;
+            }
+
+            WriteLogHex(item);*/
+        }
+
+        private IEnumerable<byte> GetBytesFromByteString(string s)
+        {
+            for (int index = 0; index < s.Length; index += 2)
+            {
+                yield return Convert.ToByte(s.Substring(index, 2), 16);
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void buttonClearLogs_Click(object sender, EventArgs e)
         {
-
+            dataGridViewLogs.Rows.Clear();
+            dataGridViewLogs.Refresh();
+            SetDataGridLogHeaders();
         }
-
+        
         private void connectToServer_Click(object sender, EventArgs e)
         {
-            startListeningServer();
+            if (buttonOpenCloseServer.Text=="Включить сервер") 
+                changeServerState(true);
+            else
+                changeServerState(false);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName == "" || Path.GetExtension(openFileDialog1.FileName).ToLower() != ".txt")
+                return;
+            string path = openFileDialog1.FileName;
+            byte[] dataArray=File.ReadAllBytes(path);
+            List<byte[]> data = new List<byte[]>();
+            int dataArrayLength = dataArray.Length;
+            int countDataPacket = (int)(dataArrayLength / 255);
+            for (int index = 0; index <= countDataPacket; index++)
+            {
+                int subArrayLength = (index!= countDataPacket) ? 255 : dataArrayLength % 255;
+                byte[] subArray = new byte[subArrayLength];
+
+                Array.Copy(dataArray, subArray, subArrayLength);
+                data.Add(subArray);
+            }
+            int i = 0;
+
+
         }
     }
 }
